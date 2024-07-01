11277d374b4cdb58027b5bbb25e11f0ae3bd117c
grentank <mexmat100394@gmail.com>
2024-07-01 12:07:33 +0300

    initial commit

--- generateAddresses.js
+++
@@ -1,9 +0,0 @@
-const { fakerRU: faker } = require('@faker-js/faker');
-
-function generateAddresses(num) {
-  return new Array(num) // new создаёт новый объект
-    .fill(null)
-    .map(() => faker.location.streetAddress());
-}
-
-module.exports = generateAddresses;
